a4cb5ca740644cb5f8a37d1abdd9c6e28f58303e
Grant Z <grantzyr@gmail.com>
2020-07-01 22:19:57 -0700

    Remove comments and add function SortByName() (sort by restaurant name)

--- app/src/main/java/com/example/cmpt276project/model/RestaurantManager.java
+++ app/src/main/java/com/example/cmpt276project/model/RestaurantManager.java
@@ -2,6 +2,8 @@ package com.example.cmpt276project.model;
 import androidx.annotation.NonNull;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -22,36 +24,34 @@ public class RestaurantManager implements Iterable<Restaurant>{
         // Ensure for singleton
     }
 
-//    private int length=0;
-//    private HashMap<String, Restaurant> hashMap = new HashMap<>();
-
     public int getLength() {
         return restaurants.size();
     }
     public void addRestaurant(Restaurant restaurant) {
-        // Array List implementation
         restaurants.add(restaurant);
-
-//        // HashMap Implementation
-//        hashMap.put(name, restaurant);
-//        length ++;
     }
     public Restaurant get(int position) {
-        // Array List implementation
         return restaurants.get(position);
-
-        // Hash Map implementation
-//        hashMap.get(name);
     }
 
     public void removeRestaurant(Restaurant restaurant) {
-        // Array List implementation
         restaurants.remove(restaurant);
 
-        // Hash Map implementation
-//        hashMap.remove(name);
-//        length--;
     }
+
+    static class SortByName implements Comparator<Restaurant> {
+        @Override
+        public int compare(Restaurant o1, Restaurant o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+
+
+
+
+
+
+
 
     @Override
     public Iterator<Restaurant> iterator() {
