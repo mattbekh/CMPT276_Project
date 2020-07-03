9cc2f31f10721950002f9b0974bbab7b915313cd
Grant Z <grantzyr@gmail.com>
2020-07-02 19:46:38 -0700

    1. add restaurants_itr1.csv 2. add an function in the RestaurantManager to read the csv and save the data in instance

--- app/src/main/java/com/example/cmpt276project/model/RestaurantManager.java
+++ app/src/main/java/com/example/cmpt276project/model/RestaurantManager.java
@@ -1,6 +1,17 @@
 package com.example.cmpt276project.model;
+import android.content.Context;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
+import com.example.cmpt276project.App;
+import com.example.cmpt276project.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -10,6 +21,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class RestaurantManager implements Iterable<Restaurant>{
     private List<Restaurant> restaurants = new ArrayList<>();
 
@@ -18,12 +30,54 @@ public class RestaurantManager implements Iterable<Restaurant>{
     public static RestaurantManager getInstance(){
         if(instance == null){
             instance = new RestaurantManager();
+            readRestaurantData();
         }
         return instance;
     }
 
     private RestaurantManager(){
         // Ensure for singleton
+    }
+
+    private static void readRestaurantData() {
+        InputStream is = App.resources().openRawResource(R.raw.restaurants_itr1);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+        String line;
+        while(true) {
+            try {
+                // step over headers
+                reader.readLine();
+
+                while ((line = reader.readLine()) != null) {
+                    // split by ','
+                    String[] tokens = line.split(",");
+
+                    // read the data
+                    Restaurant tmpData = new Restaurant(
+                            // String trackingNumber
+                            tokens[0],
+                            // String name
+                            tokens[1],
+                            // String address,
+                            tokens[2],
+                            // String city
+                            tokens[3],
+                            // double latitude
+                            Double.parseDouble(tokens[5]),
+                            // double longitude
+                            Double.parseDouble(tokens[6])
+                            );
+
+                    // put data into instance
+                    instance.addRestaurant(tmpData);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     public int getLength() {
@@ -51,13 +105,6 @@ public class RestaurantManager implements Iterable<Restaurant>{
             return o1.getName().compareTo(o2.getName());
         }
     }
-
-
-
-
-
-
-
 
     @Override
     public Iterator<Restaurant> iterator() {
