d9cca426ffce0d5b296bdc41540a1ce93b976573
mattbekh <mbekhter@sfu.ca>
2020-07-13 16:29:24 -0700

    Program fetches modification date from server, stores date to sharedprefs if more recent than currently stored date (or if none)
    To Do: Download CSV data and store locally

--- app/src/main/java/com/example/cmpt276project/model/DataHandler.java
+++ app/src/main/java/com/example/cmpt276project/model/DataHandler.java
@@ -1,47 +1,115 @@
 package com.example.cmpt276project.model;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.os.Environment;
 import android.util.Log;
-
-import com.opencsv.CSVWriter;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
-public class DataHandler extends AsyncTask<Void,Void,Void> {
+import static java.lang.Integer.parseInt;
+
+public class DataHandler extends AsyncTask<URL, Void, Boolean> {
 
     private Context mContext;
-    public DataHandler(Context context) {
+
+    private int year;
+    private int month;
+    private int day;
+
+    public DataHandler(Context context){
         mContext = context;
-        Log.v("DataHandler","This is bullshit");
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
-        //TODO make URL dynamic
+    protected Boolean doInBackground(URL... voids) {
+        if(updateNeeded()){
+            downloadCSVData("restaurant_data");
+        }
+        return updateNeeded();
+    }
+
+    private void downloadCSVData(String fileName) {
+
+    }
+
+    /**
+     * Compares stored pref date to Modification Date fetched from URL
+     * returns True if Update is needed
+      */
+
+    private Boolean updateNeeded() {
+
+        if(getModifiedDate()!=null){
+            if(getModDateSharedPrefs().equals("None")){
+                storeModDateSharedPrefs(getModifiedDate());
+                return true;
+            }
+            else{
+                if(convertToDate(getModDateSharedPrefs()).compareTo(convertToDate(getModifiedDate()))<0){
+                    storeModDateSharedPrefs(getModifiedDate());
+                    return true;
+                }
+                else{
+                    //do nothing
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private void storeModDateSharedPrefs(String date) {
+        SharedPreferences prefs = mContext.getSharedPreferences("AppData",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        editor.putString("ModificationDate",date);
+        editor.apply();
+    }
+
+    private String getModDateSharedPrefs() {
+        SharedPreferences prefs = mContext.getSharedPreferences("AppData",Context.MODE_PRIVATE);
+
+        return prefs.getString("ModificationDate","None");
+    }
+
+    private String getModifiedDate() {
+
+        URL mUrl = null;
+        String[] content;
         try {
-            URL url = new URL("http://data.surrey.ca/api/3/action/package_show?id=restaurants");
-
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            InputStream inputStream = httpURLConnection.getInputStream();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-            Log.v("DataHandler","Reached doInBackground");
-            writeCSV();
-
+            mUrl = new URL("https://data.surrey.ca/api/3/action/package_show?id=restaurants");
         } catch (MalformedURLException e) {
             e.printStackTrace();
+        }
+        try {
+            assert mUrl != null;
+            URLConnection connection = mUrl.openConnection();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            String line = br.readLine();
+            String search = "metadata_modified";
+
+            content = line.split(",");
+
+            for(int i=0; i<content.length;i++){
+
+                if(content[i].toLowerCase().contains(search.toLowerCase())){
+                    // Found line
+                    // Modify text to output only integers, which will later be truncated for yyyy,MM,dd
+                    return content[i].replaceAll("[^0-9]", "");
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -49,36 +117,15 @@ public class DataHandler extends AsyncTask<Void,Void,Void> {
         return null;
     }
 
-    private void writeCSV() throws IOException {
-        Log.d("DataHandler","Reached writeCSV");
-        URL restaurantURL = new URL("http://data.surrey.ca/api/3/action/package_show?id=restaurants");
-
-        HttpURLConnection httpURLConnection = (HttpURLConnection) restaurantURL.openConnection();
-        InputStream inputStream = httpURLConnection.getInputStream();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        // Line to read CSV
-        String fileName = "restaurant_data.csv";
-        File file = new File(mContext.getFilesDir() + fileName);
-
-
-        CSVWriter writer = new CSVWriter(new FileWriter(file));
-
-        String line = "";
-        while((line = bufferedReader.readLine()) != null){
-
-            line = bufferedReader.readLine();
-
-            String[] comps = line.split(",");
-
-            Log.d("DataHandler","Value of line is :" +line);
-            writer.writeNext(comps);
-        }
-        writer.close();
-
+    private GregorianCalendar convertToDate(String numberOnly) {
+        year = parseInt(numberOnly.substring(0,4));
+        month = parseInt(numberOnly.substring(4,6));
+        day = parseInt(numberOnly.substring(6,8));
+        return new GregorianCalendar(year, month, day);
     }
 
     @Override
-    protected void onPostExecute(Void aVoid) {
-        super.onPostExecute(aVoid);
+    protected void onPostExecute(Boolean update) {
     }
 }
+
