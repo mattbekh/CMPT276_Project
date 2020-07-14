eb76c58f03f4228e1731a87923ac71f49acd7544
mattbekh <mbekhter@sfu.ca>
2020-07-14 13:07:45 -0700

    Program fetches modification date from server, stores date to sharedprefs if more recent than currently stored date (or if none)
    Feature downloads CSV from URL and stores in Internal Storage, currently invisible to user in download folder
    
    Download Manager now sends notification that download has been completed
    
    TO DO: Check if file is useable by program

--- app/src/main/java/com/example/cmpt276project/ui/OpeningScreenActivity.java
+++ app/src/main/java/com/example/cmpt276project/ui/OpeningScreenActivity.java
@@ -3,45 +3,58 @@ package com.example.cmpt276project.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteConstraintException;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.cmpt276project.R;
 import com.example.cmpt276project.model.DataHandler;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 /**
  * This class plays a small animation before launching the main app
  */
 public class OpeningScreenActivity extends AppCompatActivity {
 
+    private static String RESTAURANTS_URL = "http://data.surrey.ca/api/3/action/package_show?id=restaurants";
+    private static String INSPECTIONS_URL = "http://data.surrey.ca/api/3/action/package_show?id=fraser-health-restaurant-inspection-reports";
+
+    private static String RESTAURANTS_CSV_URL = "https://data.surrey.ca/dataset/3c8cb648-0e80-4659-9078-ef4917b90ffb/resource/0e5d04a2-be9b-40fe-8de2-e88362ea916b/download/restaurants.csv";
+    private static String INSPECTIONS_CSV_URL = "https://data.surrey.ca/dataset/948e994d-74f5-41a2-b3cb-33fa6a98aa96/resource/30b38b66-649f-4507-a632-d5f6f5fe87f1/download/fraserhealthrestaurantinspectionreports.csv";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
-
+        loadCSVData();
         // Set up handler for delay of Main Menu
         Runnable r = new Runnable() {
             @Override
             public void run() {
-                DataHandler dataHandler = new DataHandler(OpeningScreenActivity.this);
-                dataHandler.downloadCSVData("https://data.surrey.ca/dataset/3c8cb648-0e80-4659-9078-ef4917b90ffb/resource/0e5d04a2-be9b-40fe-8de2-e88362ea916b/download/restaurants.csv","restaurant_data.csv");
-                File file = new File(Environment.
-                        DIRECTORY_DOWNLOADS, "restaurant_data.csv");
-                Toast.makeText(OpeningScreenActivity.this,
-                        file.toString(),
-                        Toast.LENGTH_LONG).show();
-//                launchMainMenu();
-//
-//                finish();
+                launchMainMenu();
+                finish();
             }
         };
 
         Handler handler = new Handler();
-        handler.postDelayed(r,10000);
+        handler.postDelayed(r,3200);
+    }
+
+    private void loadCSVData() {
+        DataHandler dataHandler = new DataHandler(OpeningScreenActivity.this);
+        dataHandler.downloadCSVData(RESTAURANTS_CSV_URL,"restaurant_data.csv");
+
+//        dataHandler.clearSharedPrefs();
     }
 
     private void launchMainMenu() {
