2c2a3a0a8db1b454dadc61cb72e309de9f9153d3
Grant Z <grantzyr@gmail.com>
2020-07-21 09:24:22 -0700

    added class level comment

--- app/src/main/java/com/example/cmpt276project/model/AllRestaurant.java
+++ app/src/main/java/com/example/cmpt276project/model/AllRestaurant.java
@@ -4,30 +4,34 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.maps.android.clustering.ClusterItem;
 
 /**
- *  This class
+ *  This class is used for clustering
  * */
 
 public class AllRestaurant implements ClusterItem {
     private final LatLng mPosition;
     private final String mTitle;
     private final String mSnippet;
+    private final String mHazard;
 
     public AllRestaurant(double lat, double lng) {
         mPosition = new LatLng(lat, lng);
         mTitle = "";
         mSnippet = "";
+        mHazard = "";
     }
 
     public AllRestaurant(double lat, double lng, String title) {
         mPosition = new LatLng(lat, lng);
         mTitle = title;
         mSnippet = "";
+        mHazard = "";
     }
 
-    public AllRestaurant(double lat, double lng, String title, String snippet) {
+    public AllRestaurant(double lat, double lng, String title, String snippet, String hazard) {
         mPosition = new LatLng(lat, lng);
         mTitle = title;
         mSnippet = snippet;
+        mHazard = hazard;
     }
 
     @Override
