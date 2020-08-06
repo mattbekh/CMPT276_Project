1767c3ad4136677d932fdf2fcdd642dce5af59b0
ronny292 <rad8@sfu.ca>
2020-08-05 22:57:49 -0700

    Fix bug where marker info would not display when navigated to from restaurant

--- app/src/main/java/com/example/cmpt276project/model/Restaurant.java
+++ app/src/main/java/com/example/cmpt276project/model/Restaurant.java
@@ -1,16 +1,19 @@
 package com.example.cmpt276project.model;
 
-import androidx.annotation.NonNull;
+import android.content.res.Resources;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.GregorianCalendar;
-import java.util.Iterator;
+import androidx.annotation.Nullable;
+
+import com.example.cmpt276project.App;
+import com.example.cmpt276project.R;
+import com.example.cmpt276project.model.database.DatabaseManager;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
 
 /**
  * This class represents a restaurant and all the inspections it has been subject to.
  */
-public class Restaurant {
+public class Restaurant implements ClusterItem {
 
     public enum RestaurantName { MCDONALDS, WENDYS, BLENZ, PIZZAHUT, AW, TIMS, STARBUCKS, ELEVEN, BOSTON, SUBWAY, UNKNOWN }
 
@@ -21,6 +24,7 @@ public class Restaurant {
     private double latitude;
     private double longitude;
     private boolean isFavourite = false;
+    private Inspection mostRecentInspection;
 
     public Restaurant(String id,
                       String name,
@@ -37,6 +41,7 @@ public class Restaurant {
         this.latitude = latitude;
         this.longitude = longitude;
         this.isFavourite = favourite == 1;
+        this.mostRecentInspection = null;
     }
 
     public String getId() {
@@ -61,6 +66,54 @@ public class Restaurant {
 
     public double getLongitude() {
         return longitude;
+    }
+
+    public String getHazardResource() {
+        if (mostRecentInspection == null) {
+            DatabaseManager dbManager = DatabaseManager.getInstance();
+            mostRecentInspection = dbManager.getMostRecentInspection(id);
+        }
+        if (mostRecentInspection == null) {
+            return "hazard_unknown";
+        }
+        switch (mostRecentInspection.getHazardRating()) {
+            case LOW:
+                return "hazard_low";
+            case MODERATE:
+                return "hazard_mid";
+            case HIGH:
+                return "hazard_high";
+            default:
+                return "hazard_unknown";
+        }
+    }
+
+    @Nullable
+    @Override
+    public LatLng getPosition() {
+        return new LatLng(latitude, longitude);
+    }
+
+    @Nullable
+    @Override
+    public String getTitle() {
+        return name;
+    }
+
+    @Nullable
+    @Override
+    public String getSnippet() {
+        if (mostRecentInspection == null) {
+            DatabaseManager dbManager = DatabaseManager.getInstance();
+            mostRecentInspection = dbManager.getMostRecentInspection(id);
+        }
+        Resources res = App.resources();
+        String noInspections = res.getString(R.string.Inspection_no_inspections_found);
+        String hazardLevel = res.getString(R.string.Inspection_hazard_level);
+        if (mostRecentInspection == null) {
+           return String.format("%s %s%s", address, hazardLevel, noInspections);
+        }
+        return String.format("%s %s%s", address, hazardLevel, mostRecentInspection.getHazardRatingString());
     }
 
     public boolean isFavourite() {
