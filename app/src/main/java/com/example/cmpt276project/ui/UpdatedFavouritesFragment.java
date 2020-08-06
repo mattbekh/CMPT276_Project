44aaefa8ef0c7edcba680a8f6023bc19d95734cd
Grant Z <grantzyr@gmail.com>
2020-08-05 18:59:40 -0700

    sample fragment

--- app/src/main/java/com/example/cmpt276project/ui/UpdatedFavouritesFragment.java
+++ app/src/main/java/com/example/cmpt276project/ui/UpdatedFavouritesFragment.java
@@ -1,37 +1,24 @@
 package com.example.cmpt276project.ui;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
-
-import com.airbnb.lottie.parser.IntegerParser;
 import com.example.cmpt276project.R;
-import com.example.cmpt276project.model.Inspection;
 import com.example.cmpt276project.model.RestaurantManager;
-import com.example.cmpt276project.model.database.RestaurantFilter;
-
 import java.util.HashSet;
 import java.util.logging.Level;
 
 public class UpdatedFavouritesFragment extends AppCompatDialogFragment {
-    private RestaurantManager manager;
-    private boolean isFavourites;
 
     @NonNull
     @Override
